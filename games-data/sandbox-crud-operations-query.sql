47ca80bef4f98df03c3949222571f1ef7b9481a8
Xavierg15 <xbossftw@outlook.com>
2022-11-22 21:35:02 -0600

    Query project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value,string_value,datetime_value) VALUES (2046, 'goodbye guys','1918-03-26 02:02:02');
+
+--UPDATE sandbox SET int32_value = -103675 WHERE id = 77;
+
+--DELETE FROM sandbox WHERE id = 85;
+
+SELECT * FROM sandbox;
